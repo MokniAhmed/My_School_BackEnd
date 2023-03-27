16dc458bf72dd9258c3be7c4c0a4e7e7cbbf55b0
a7me3D <ahmedjbeli54@yahoo.fr>
2023-03-27 23:10:33 +0100

    change file name

--- src/queries/user.query.ts
+++ src/queries/user.query.ts
@@ -1,7 +1,7 @@
 import get from "crud/get";
 import list from "crud/list";
-import { GraphQLID, GraphQLString } from "graphql";
-import { Role, User } from "models/user.model";
+import { GraphQLString } from "graphql";
+import { User } from "models/user.model";
 import { UserType } from "types/user.type";
 
 export default {
